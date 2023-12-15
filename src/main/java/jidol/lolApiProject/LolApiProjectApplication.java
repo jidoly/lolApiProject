2d25f9d097156deadc6e022293234048af6caa97
jidoly <jidol8849@gmail.com>
2023-12-15 15:17:40 +0900

    test : test commit - project base

--- src/main/java/jidol/lolApiProject/LolApiProjectApplication.java
+++ src/main/java/jidol/lolApiProject/LolApiProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = "jidol.lolApiProject.api")
 @SpringBootApplication
-public class LolApiProjectApplication {
+public class LolApiProjectApplication { // 커밋테스트
 
 	public static void main(String[] args) {
 		SpringApplication.run(LolApiProjectApplication.class, args);
